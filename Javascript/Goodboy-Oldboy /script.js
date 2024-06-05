a939551d381474db48cef4d4e0e8e8cbb133f16b
Noah <lorians995@gmail.com>
2024-06-05 16:58:58 +0200

    fixed code repeat

--- Javascript/Goodboy-Oldboy /script.js
+++ Javascript/Goodboy-Oldboy /script.js
@@ -1,18 +1,19 @@
-let dogYearBirth = 2014;
-let dogYearFuture = 2031;
-let shouldShowResultInDogYears = true;
+const dogYearBirth = 2014;
+const dogYearFuture = 2031;
+const shouldShowResultInDogYears = false;
 
-let dogAge;
-if (shouldShowResultInDogYears) {
-  dogAge = dogYearFuture - dogYearBirth;
-} else {
-  dogAge = (dogYearFuture - dogYearBirth) * 7;
-}
+const dogAgeInHumanYears = dogYearFuture - dogYearBirth;
+
+const dogAgeInDogYears = dogAgeInHumanYears * 7;
 
 if (shouldShowResultInDogYears) {
-  console.log(`Your dog will be ${dogAge} dog years old in ${dogYearFuture}`);
+  console.log(
+    `Your dog will be ${dogAgeInDogYears} dog years old in ${dogYearFuture}`
+  );
 } else {
-  console.log(`Your dog will be ${dogAge} human years old in ${dogYearFuture}`);
+  console.log(
+    `Your dog will be ${dogAgeInHumanYears} human years old in ${dogYearFuture}`
+  );
 }
 
 // Like before lets create three variables but this time we call them dogYearOfBirth, dogYearFuture and dogYear. We add an extra variable called shouldShowResultInDogYears. If it is true we should show the result in dog years, if it is false we should show it in human years. What do we call this type of variable? Now dependent on shouldShowResultInDogYears log this string out: "Your dog will be 10 human years old in 2027" or "Your dog will be 70 dog years old in 2027"
