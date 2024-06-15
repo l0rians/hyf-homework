1065b3d7b27d2b6ae9f87dd62b6c96ddcea17389
Noah <lorians995@gmail.com>
2024-06-15 06:34:18 +0200

    changed monday to sunday and cleaned code

--- Javascript_2/Event application/script.js
+++ Javascript_2/Event application/script.js
@@ -1,23 +1,22 @@
 function getEventWeekday(daysFromToday) {
   const weekDays = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
   ];
 
   const today = new Date();
 
   let todayIndex = today.getDay();
 
-  todayIndex = (todayIndex + 6) % 7;
   const eventDayIndex = (todayIndex + daysFromToday) % 7;
 
   return weekDays[eventDayIndex];
 }
-console.log(getEventWeekday(5)); // 5 days from today
+console.log(getEventWeekday(1)); // 5 days from today
 console.log(getEventWeekday(9)); // 9 days from today
 console.log(getEventWeekday(3)); // 3 days from today
