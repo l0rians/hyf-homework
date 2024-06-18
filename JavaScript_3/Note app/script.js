afb6254a7d2727013778fb9a2374a3f0a1d6310b
Noah <lorians995@gmail.com>
2024-06-18 11:58:10 +0200

    added formatted log out for notes

--- JavaScript_3/Note app/script.js
+++ JavaScript_3/Note app/script.js
@@ -8,6 +8,8 @@ function saveNote(content, id) {
 saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
 
+// console.log(notes);
+
 // function to get the note by its id
 function getNote(id) {
   // checking if is id number, if not, then error
@@ -21,8 +23,21 @@ function getNote(id) {
       return notes[i];
     }
   }
-  console.log("Note with that ID not found");
+  //console.log("Note with that ID not found");
 }
 const getNoteId = getNote(4);
 
-console.log(getNoteId);
+//console.log(getNoteId);
+
+function logOutNotesFormatted() {
+  // goint through all array
+  for (let i = 0; i < notes.length; i++) {
+    console.log(
+      `The note with id: ${notes[i].id}, has the following note text: ${notes[i].content}`
+    );
+  }
+}
+
+logOutNotesFormatted();
+// The note with id: 1, has the following note text: Pick up groceries
+// The note with id: 2, has the following note text: Do laundry
