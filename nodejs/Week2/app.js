13149024613c4e0a99edf5f223509f8eb469d559
Noah <lorians995@gmail.com>
2024-09-24 23:25:57 +0200

    added POST /search

--- nodejs/Week2/app.js
+++ nodejs/Week2/app.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 import fs from "fs";
+app.use(express.json());
 
 // Support parsing JSON requests
 const documents = JSON.parse(fs.readFileSync("documents.json", "utf8"));
@@ -24,6 +25,42 @@ app.get("/search", (req, res) => {
   res.json(filteredDocuments);
 });
 
+app.post("/search", (req, res) => {
+  const { q } = req.query;
+  const { fields } = req.body;
+
+  // if both q and fields are provided, return an error
+  if (q && fields) {
+    return res
+      .status(400)
+      .send("Cannot search by both query and fields simultaneously");
+  }
+  // if q is provided, search by query
+  if (q) {
+    const filteredDocuments = documents.filter((doc) =>
+      Object.values(doc).some((value) =>
+        value.toString().toLowerCase().includes(q.toLowerCase())
+      )
+    );
+    return res.json(filteredDocuments);
+  }
+
+  // search by fields
+  if (fields) {
+    const filteredDocuments = documents.filter((doc) => {
+      return Object.entries(fields).every(
+        ([key, value]) =>
+          doc[key] &&
+          doc[key].toString().toLowerCase() === value.toString().toLowerCase()
+      );
+    });
+    return res.json(filteredDocuments);
+  }
+
+  // if q and fields are not provided, return all documents
+  res.json(documents);
+});
+
 app.get("/documents/:id", (req, res) => {
   const { id } = req.params;
   const document = documents.find((doc) => doc.id == id);
@@ -34,7 +71,6 @@ app.get("/documents/:id", (req, res) => {
 
   res.json(document);
 });
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
