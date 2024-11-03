574c3d708638ed4a4fcc6dd6b2b0a87675cf440a
Noah <lorians995@gmail.com>
2024-11-03 10:28:30 +0100

    mentor's suggestion

--- react/nasa-picture-of-the-day/app/blogs/[slug]/page.js
+++ react/nasa-picture-of-the-day/app/blogs/[slug]/page.js
@@ -1,13 +1,14 @@
+"use client";
 import { usePathname } from "next/navigation";
 
 export default function BlogPost() {
-  const pathname = usePathname();
-  const title = pathname.split("/").pop().replace(/-/g, " ");
-}
+  const postSlug = usePathname().split("/").pop();
+  const title = postSlug.replace(/-/g, " ");
 
-return (
-  <div>
-    <h1>{title.charAt(0).toUpperCase() + title.slice(1)}</h1>
-    <p>This is the content of {title}.</p>
-  </div>
-);
+  return (
+    <div>
+      <h1>{title.charAt(0).toUpperCase() + title.slice(1)}</h1>
+      <p>This is the content of {title}.</p>
+    </div>
+  );
+}
